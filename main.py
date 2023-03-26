fc0bf8467747228c02ee2e5bde092a4c15cf603d
Hank <hz4514261@gmail.com>
2023-03-26 18:57:14 +0800

    fix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import grpc,AtheNAC_pb2,AtheNAC_pb2_grpc,datetime,time
 from google.protobuf.timestamp_pb2 import Timestamp
 
+Date_to_timestamp = Timestamp()
+Date_to_timestamp.FromDatetime(datetime.datetime(1970,1,1,0,0,0))
 
 channel = grpc.insecure_channel('192.168.13.170:18002')
 stub = AtheNAC_pb2_grpc.HostAgentServiceStub(channel)
@@ -12,19 +14,15 @@ request = AtheNAC_pb2.HostReportRequest(
     os_type='windows',
     os_desc='WindowsTest',
     share_folder=[],
-    pending_hot_fix=[],
-    windows_hot_fix_last_check_time=None,
+    pending_hot_fix=['Hotfix 123456 - KB123456','Hotfix 666666 - KB666666'],
+    windows_hot_fix_last_check_time=Date_to_timestamp,
     local_admin_account=[],
-    timestamp=None,
+    timestamp=Date_to_timestamp,
     motherboard_serial_number='Test_Motherboard',
     softwares=[{"DisplayName": "Hank_Program_hehe","DisplayVersion": "7.8.8.10","Publisher": "HankMaster","InstallDate": "20230101","EstimatedSize":"888"}],
-    AgentLastCheckTime=None
+    AgentLastCheckTime=Date_to_timestamp
 )
 
-Date_to_timestamp = Timestamp()
-Date_to_timestamp.FromDatetime(datetime.datetime(1970,1,1,0,0,0))
-request.timestamp.CopyFrom(Date_to_timestamp)
-request.AgentLastCheckTime.CopyFrom(Date_to_timestamp)
-request.windows_hot_fix_last_check_time.CopyFrom(Date_to_timestamp)
+
 respond = stub.SendHostReport(request)
 print(f'AtheNAC reply message {respond}')
