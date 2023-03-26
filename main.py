464a98298154bced797cc228199b163b8144cef7
Hank <hz4514261@gmail.com>
2023-03-26 18:49:06 +0800

    fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import grpc,AtheNAC_pb2,AtheNAC_pb2_grpc,datetime,time
 from google.protobuf.timestamp_pb2 import Timestamp
 
-pass
+
 channel = grpc.insecure_channel('192.168.13.170:18002')
 stub = AtheNAC_pb2_grpc.HostAgentServiceStub(channel)
 request = AtheNAC_pb2.HostReportRequest(
@@ -17,15 +17,14 @@ request = AtheNAC_pb2.HostReportRequest(
     local_admin_account=[],
     timestamp=None,
     motherboard_serial_number='Test_Motherboard',
-    softwares=[],
+    softwares=[{"DisplayName": "Hank_Program_hehe","DisplayVersion": "7.8.8.10","Publisher": "HankMaster","InstallDate": "20230101","EstimatedSize":"888"}],
     AgentLastCheckTime=None
 )
 
-timestampaa = Timestamp()
-timestampaa.FromDatetime(datetime.datetime.now())
-request.timestamp.CopyFrom(timestampaa)
-request.AgentLastCheckTime.CopyFrom(timestampaa)
-request.windows_hot_fix_last_check_time.CopyFrom(timestampaa)
-
+Date_to_timestamp = Timestamp()
+Date_to_timestamp.FromDatetime(datetime.datetime(1970,1,1,0,0,0))
+request.timestamp.CopyFrom(Date_to_timestamp)
+request.AgentLastCheckTime.CopyFrom(Date_to_timestamp)
+request.windows_hot_fix_last_check_time.CopyFrom(Date_to_timestamp)
 respond = stub.SendHostReport(request)
-pass
+print(f'AtheNAC reply message {respond}')
